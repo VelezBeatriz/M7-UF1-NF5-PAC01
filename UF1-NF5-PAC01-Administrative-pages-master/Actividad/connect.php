88c6c41fbeb13ac685760bb7f3205fc1c8c7c87b
Beatriz Vélez <145451383+VelezBeatriz@users.noreply.github.com>
2023-11-09 11:59:27 +0100

    Init Commit

---
+++ UF1-NF5-PAC01-Administrative-pages-master/Actividad/connect.php
@@ -0,0 +1,20 @@
+<?php
+//Definir constantes de utilidad
+define( 'SITE_DDBB', 'localhost');
+define( 'ADMIN_USER', 'root');
+define( 'ADMIN_PASSWORD', 'root');
+define( 'DATABASE_NAME', 'moviesite');
+define( 'URL_SITE', 'http://localhost:8080/Tables/M7_NF3-PAC03-RESE%c3%91AS/');
+
+//Conexión a la BBDD
+$bbdd = @new mysqli(SITE_DDBB, ADMIN_USER, ADMIN_PASSWORD, DATABASE_NAME);
+
+    if($bbdd->connect_errno){
+        die('Error de Connexión número ' . $bbdd->connect_errno . ', ' . $bbdd->connect_error);
+    } else {
+        ?>
+            <p>¡Connexión establecida con éxito, navega a tu gusto!</p>
+        <?php
+    }
+
+?>
