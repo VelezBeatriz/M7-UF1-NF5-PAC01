bd7db2be3c6d32f3e37ffeb711fe1bcdeeed53e3
VelezBeatriz <velezsanchezbeatriz@fpllefia.com>
2023-11-15 22:43:25 +0100

    Cambio nombre de directorios

--- VelezBeatrizDeberes/connect.php
+++ VelezBeatrizDeberes/connect.php
@@ -5,8 +5,8 @@ define( 'ADMIN_USER', 'root');
 define( 'ADMIN_PASSWORD', 'root');
 define( 'DATABASE_NAME', 'moviesite');
 define( 'URL', $_SERVER['REQUEST_URI']);
-define( 'URL_SITE', "http://localhost:8080/Deberes/");
-define( 'URL_RETURN', "http://localhost:8080/Deberes/index.php");
+define( 'URL_SITE', "http://localhost:8080/VelezBeatrizDeberes/");
+define( 'URL_RETURN', "http://localhost:8080/VelezBeatrizDeberes/index.php");
 
 
 //Conexión a la BBDD
